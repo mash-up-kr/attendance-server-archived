de71e9e6265e6b37cfef46db04bdd148afcb40ba
devheee <heee7897@gmail.com>
2022-02-20 14:58:51 +0900

    Initial commit

---
+++ mashup-admin/src/main/java/kr/mashup/attendance/AttendanceAdminApplication.java
@@ -0,0 +1,11 @@
+package kr.mashup.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceAdminApplication.class, args);
+    }
+}
---
+++ mashup-api/src/main/java/kr/mashup/attendance/AttendanceApiApplication.java
@@ -0,0 +1,11 @@
+package kr.mashup.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceApiApplication.class, args);
+    }
+}
